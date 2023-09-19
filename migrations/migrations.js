cd76faa2be1ef6ca6372ea1657aa9cbf6d8cda9d
debian <lucho.2012.tandil@gmail.com>
2023-09-19 02:09:46 +0200

    solucion a conflictos

--- migrations/migrations.js
+++ migrations/migrations.js
@@ -180,15 +180,12 @@ exports.do_migrations = async function () {
     { id: uuid.v4(), id_permiso: 'MASCOTAS_C_USER_EDIT', id_rol: 'DUENIO_MASCOTA', fecha_creado: new Date(), fecha_modificado: new Date() }
     ,{ busqueda_ignorar_campo: ['fecha_creado', 'fecha_modificado', 'id'] })
 
-  if (! await hasTable("usuarios_roles")) {
-    console.log("Creo tabla usuarios_roles")
-    await global.knex.schema.createTable('usuarios_roles', function (table) {
-      table.primary(["id"])
-      table.string('id', 36)
-      table.string('id_usuario',36)
-      table.string('id_rol',36)
-      table.datetime('fecha_creado')
-      table.datetime('fecha_modificado')
+  if (! await hasTable("configuraciones")) {
+    console.log("Creo tabla configuraciones")
+    await global.knex.schema.createTable('configuraciones', function (table) {
+      table.primary(["clave"])
+      table.string('clave', 36)
+      table.string('valor',255)
     })
   }
 
@@ -251,6 +248,16 @@ exports.do_migrations = async function () {
       table.datetime('fecha_creado')
     })
   }
+  await addRecordIfNotExist("configuraciones", { clave: 'ultimo_num_mascota', valor: '0' },
+    { busqueda_ignorar_campo: ['valor'] })
+
+  if (!await hasColumn('mascotas_registradas', 'cod_qr')){
+    console.log("Creo columna cod_qr en mascotas_registradas")
+    await knex.schema.alterTable('mascotas_registradas', function (table) {
+      table.string('cod_qr').comment("Usado en generación de código QR")
+    })
+  }
+  
 }
 
 async function hasColumn(table_name, column) {
